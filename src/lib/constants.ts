b36a95ee430cc55e8e41f5c8cb1f704f8ed255e2
Darren Carlin <dazftw@gmail.com>
2025-08-28 22:52:12 -0400

    fix: update BASE_URL to use NODE_ENV for production environment

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,7 @@
-export const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://www.mypickleballmap.com"
+    : "http://localhost:3000";
 
 export const COURTS_QUERY_KEY = "courts";
 
